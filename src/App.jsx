d4a80fffb70a5ea91463bfbcdde74d37bf1643d3
Dushmanta <dushmanta.dev@gmail.com>
2023-11-02 23:24:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import Heading from './components/Heading'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
