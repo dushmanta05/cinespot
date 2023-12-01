90abac3a4b7beb00b53bd4599a77a4339973b210
Dushmanta <dushmanta0511@gmail.com>
2023-12-01 23:58:37 +0530

    update movie section, format code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="container d-flex justify-content-center mt-5">
+      Built with React
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/TopMovies.jsx
+++ src/components/TopMovies.jsx
@@ -9,12 +9,17 @@ import shawshankImg from "../assets/images/thumbnail/shawshank.jpg";
 import Card from "./Card";
 
 const arrImg = [
-  fastxImg,
-  cocoImg,
-  muzzleImg,
-  retributionImg,
-  schindlerlistImg,
-  shawshankImg,
+  { image: fastxImg, title: "Fast X", year: 2023, rating: 5.6 },
+  { image: cocoImg, title: "Coco", year: 2023, rating: 5.6 },
+  { image: muzzleImg, title: "Muzzle", year: 2023, rating: 5.6 },
+  { image: retributionImg, title: "Retribution", year: 2023, rating: 5.6 },
+  { image: schindlerlistImg, title: "Schindler List", year: 2023, rating: 5.6 },
+  {
+    image: shawshankImg,
+    title: "Shawshank",
+    year: 2023,
+    rating: 5.6,
+  },
 ];
 
 const TopMovies = () => {
@@ -22,9 +27,9 @@ const TopMovies = () => {
     <div className="container">
       <h3>Top Movies</h3>
       <div className="row">
-        {arrImg.map((img, index) => {
-          return <Card img={img} />;
-        })}
+        {arrImg.map((item, index) => (
+          <Card key={index} item={item} />
+        ))}
       </div>
     </div>
   );
